3bdb84e9ee2ec981bcf78da63a28ef3e42018730
Kunal Sharma <knlsharma678@gmail.com>
2018-10-16 16:25:06 +0530

    added till 10

---
+++ day10.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class day10 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int max = 0;
+        int count = 0;
+
+        while(n>0)
+            {
+                if(n%2==1)
+                    count++;
+                else
+                    count = 0;
+                if(max<count)
+                    max = count;
+            n=n/2;
+        }
+        System.out.println(max);
+
+    }
+}
---
+++ day9.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class day9 {
+    public static int factorial(int n){
+        if(n<=1)
+            return 1;
+        else
+            return n*factorial(n-1);
+    }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(factorial(N));
+    }
+}
